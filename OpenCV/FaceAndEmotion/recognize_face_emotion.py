a04ce19d6ff70888732cadf8d3b6089dd1cbad97
kostadin.kadiev@it-labs.com <kostadin.kadiev@it-labs.com>
2018-12-07 23:39:45 +0100

    - Merged Face3 and Emotion into one script that is recognizing face and emotion (with probabilities) - Removed detector and embedder argiments, since they same are always used

---
+++ OpenCV/FaceAndEmotion/recognize_face_emotion.py
@@ -0,0 +1,164 @@
+# USAGE
+# python recognize_face_emotion.py \
+#	--embedding-model openface_nn4.small2.v1.t7 \
+#	--recognizer output/recognizer.pickle \
+#	--le output/le.pickle
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import pickle
+import time
+import cv2
+import os
+
+#emotion packages
+from keras.models import load_model
+from statistics import mode
+from utils.datasets import get_labels
+from utils.inference import detect_faces
+from utils.inference import draw_text
+from utils.inference import draw_bounding_box
+from utils.inference import apply_offsets
+from utils.inference import load_detection_model
+from utils.preprocessor import preprocess_input
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-r", "--recognizer", required=True,
+	help="path to model trained to recognize faces")
+ap.add_argument("-l", "--le", required=True,
+	help="path to label encoder")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# load our serialized face detector from disk
+print("[INFO] loading face detector...")
+protoPath = "face_detection_model/deploy.prototxt"
+modelPath = "face_detection_model/res10_300x300_ssd_iter_140000.caffemodel"
+detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+# load our serialized face embedding model from disk
+print("[INFO] loading face recognizer...")
+embedder = cv2.dnn.readNetFromTorch("face_embedder_model/openface_nn4.small2.v1.t7")
+
+# load the actual face recognition model along with the label encoder
+recognizer = pickle.loads(open(args["recognizer"], "rb").read())
+le = pickle.loads(open(args["le"], "rb").read())
+
+# load emotion parameters and models
+emotion_labels = get_labels('fer2013')
+emotion_offsets = (20, 40)
+face_cascade = cv2.CascadeClassifier('./emotion_models/haarcascade_frontalface_default.xml')
+emotion_classifier = load_model('./emotion_models/emotion_model.hdf5')
+emotion_target_size = emotion_classifier.input_shape[1:3]
+
+# initialize the video stream, then allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+# start the FPS throughput estimator
+fps = FPS().start()
+
+# loop over frames from the video file stream
+while True:
+	# grab the frame from the threaded video stream
+	frame = vs.read()
+	# resize the frame to have a width of 600 pixels (while
+	# maintaining the aspect ratio), and then grab the image
+	# dimensions
+	frame = imutils.resize(frame, width=600)
+	(h, w) = frame.shape[:2]
+
+	# construct a blob from the image
+	imageBlob = cv2.dnn.blobFromImage(
+		cv2.resize(frame, (300, 300)), 1.0, (300, 300),
+		(104.0, 177.0, 123.0), swapRB=False, crop=False)
+
+	# apply OpenCV's deep learning-based face detector to localize
+	# faces in the input image
+	detector.setInput(imageBlob)
+	detections = detector.forward()
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with
+		# the prediction
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections
+		if confidence > args["confidence"]:
+			# compute the (x, y)-coordinates of the bounding box for
+			# the face
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# extract the face ROI
+			face = frame[startY:endY, startX:endX]
+			(fH, fW) = face.shape[:2]
+
+		    # ensure the face width and height are sufficiently large
+			if fW < 20 or fH < 20:
+			    continue
+
+			# get gray image from face and resize
+			gray_image = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+			gray_face = cv2.resize(gray_image, (emotion_target_size))
+
+			# preprocess the face for emotion
+			gray_face = preprocess_input(gray_face, True)
+			gray_face = np.expand_dims(gray_face, 0)
+			gray_face = np.expand_dims(gray_face, -1)
+
+			# predict emotion & emotion's probability
+			emotion_prediction = emotion_classifier.predict(gray_face)
+			emotion_proba = np.max(emotion_prediction)
+			emotion = emotion_labels[np.argmax(emotion_prediction)]
+
+			# construct a blob for the face ROI, then pass the blob
+			# through our face embedding model to obtain the 128-d
+			# quantification of the face
+			faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+				(96, 96), (0, 0, 0), swapRB=True, crop=False)
+			embedder.setInput(faceBlob)
+			vec = embedder.forward()
+
+			# perform classification to recognize the face
+			preds = recognizer.predict_proba(vec)[0]
+			j = np.argmax(preds)
+			name_proba = preds[j]
+			name = le.classes_[j]
+
+			# draw the bounding box of the face along with the
+			# associated probability
+			text = "{}: {:.2f}%; {}: {:.2f}%".format(name, name_proba * 100, emotion, emotion_proba * 100)
+			y = startY - 10 if startY - 10 > 10 else startY + 10
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				(0, 0, 255), 2)
+			cv2.putText(frame, text, (startX, y),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+	# update the FPS counter
+	fps.update()
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()

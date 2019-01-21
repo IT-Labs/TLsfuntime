9687ad6dfd67f9369e4757178dfb7cacdb555b44
baze.panovski <bpanovski@campusmgmt.com>
2019-01-21 16:34:39 +0100

    FR training model

--- OpenCV/FaceAndEmotion/cron_job.sh
+++ OpenCV/FaceAndEmotion/cron_job.sh
@@ -1,8 +1,21 @@
 clear
 
-echo "The scripts starts now."
+echo "The scripts starts at" $(date +'%Y-%m-%d %H:%M')
 
+echo "Setup virtual environment"
+virtualenv faceAndEmotion
+source faceAndEmotion/bin/activate
+#pip install -r requirements.txt
+
+echo "Train model"
 python extract_embeddings.py --dataset /mnt/ITLabsEmployeesPictures --embeddings output/embeddings.pickle
 python train_model.py --embeddings output/embeddings.pickle --recognizer output/recognizer.pickle --le output/le.pickle
+
+echo "Restart Face Recognition application"
+kill -9 `pgrep recognize`
+python recognize_face_emotion.py --recognizer output/recognizer.pickle --le output/le.pickle &
+
+echo "Deactivate virtual environment"
+deactivate
 
 echo "Job done."
